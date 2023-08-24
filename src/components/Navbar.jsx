279d8bdd3d42d56cf9c48474dc80df552603354c
dilato <dilato@dilatos-MacBook-Air.local>
2023-08-24 14:29:36 -0700

    Modified Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,7 @@ import { logo5, menu, close } from '../assets';
 
 const Navbar = () => {
   const [active, setActive] = useState('');
+  const [toggle,setToggle] = useState(false);
   return (
     <nav className={`${styles.padding} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
@@ -21,12 +22,27 @@ const Navbar = () => {
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
           {navLinks.map((link)=>(
-            <li>
-              <a href={`#{nav.id}`}></a>
+            <li
+              key= {link.id}
+              className={`${
+                active === link.title ? "text-white": "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={()=>setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
             </li>
           ))}
 
         </ul>
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
+          <img 
+            src={toggle? close: menu} 
+            alt="menu" 
+            className='w-[28px] h-[28px] object-contain cursor-pointer'
+            onClick={()=>setToggle(!toggle)}
+          />
+          <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}></div>
+        </div>
       </div>
     </nav>
   )
