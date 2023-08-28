3c0bcd98e2cf516f67ed705ace0ffb961979a5a6
dilato <dilato@dilatos-MacBook-Air.local>
2023-08-27 21:13:14 -0700

    added

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -2,16 +2,23 @@ import React from 'react'
 import {BallCanvas} from './canvas'
 import { SectionWrapper } from '../hoc'
 import { technologies } from '../constants'
+import { motion } from 'framer-motion'
+import { textVariant } from '../utils/motion'
+import { styles } from '../styles'
 
 const Tech = () => {
   return (
     <div className='flex flex-row flex-wrap justify-center gap-10'>
       {technologies.map((technology)=>(
-        <div className='w-28 h-28' key={technology.name}>
+        <div className='w-28 h-28 text-center' key={technology.name}>
+          <BallCanvas icon={technology.icon}></BallCanvas>
+          <motion.div variants={textVariant()}>
+            <p>{technology.name}</p>
+          </motion.div>
         </div>
       ))}
     </div>
   )
 }
 
-export default SectionWrapper(Tech,"")
+export default SectionWrapper(Tech,"tech")
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
-
+import { Tilt } from 'react-tilt'
+import {motion} from 'framer-motion'
+import {styles} from '../styles'
+import { github } from '../assets'
+import { SectionWrapper } from '../hoc'
+import { projects } from '../constants'
+import {fadeIn,textVariant} from '../utils/motion'
 const Works = () => {
   return (
-    <div>
-      Works
-    </div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>What I have done so far</p>
+        <h2 className={styles.sectionHeadText}>Work experience.</h2>
+      </motion.div>
+    </>
   )
 }
 
-export default Works
+export default SectionWrapper(Works,"");
